41697a8dd7b188f056e9a92a98bef593a57618b0
vidhyamohan93 <vidhyamohan93@gmail.com>
2026-03-24 14:55:26 +0530

    added local files

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+
+name = input("Enter your name")
+age = int(input("Enter your age"))
+
+if age>18:
+ 
+  print ("hello world ",name, "\nYour age is: " , age)
+  print ("You are eligible for Vote !!")
+
+else :
+    print ("hello world ",name, "\nYour age is: " , age)
+    print ("Ops You are not eligible for Vote !!")
